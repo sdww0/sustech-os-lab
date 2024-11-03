f4d919256935b94243eb23c931cddc6a0cdcee76
Yuke Peng <pykfirst@gmail.com>
2024-11-03 13:05:54 +0800

    Write error code to exit status pointer

--- src/syscall/wait4.rs
+++ src/syscall/wait4.rs
@@ -9,12 +9,16 @@ pub fn sys_wait4(
     wait_options: u32,
     rusage_addr: Vaddr,
 ) -> Result<SyscallReturn> {
-    debug!("[SYS_WAIT4] Wait pid: {:?}, exit_status_ptr: {:x?}, wait_options: {:x?}, rusage_addr: {:x?}",wait_pid, exit_status_ptr,wait_options,rusage_addr);
+    debug!("[SYS_WAIT4] Wait pid: {:?}, exit_status_ptr: {:x?}, wait_options: {:x?}, rusage_addr: {:x?}", wait_pid, exit_status_ptr,wait_options,rusage_addr);
     let current = current_process().unwrap();
     debug!("[SYS_WAIT4] current pid: {:?}", current.pid());
-    let (pid, _exit_code) = wait_child(wait_pid, current)?;
+    let (pid, exit_code) = wait_child(wait_pid, current.clone())?;
 
-    // TODO: Write exit code to exit_status_ptr
+    if exit_status_ptr != 0 {
+        let vm_space = current.memory_space().vm_space();
+        let mut writer = vm_space.writer(exit_status_ptr, size_of::<u32>())?;
+        writer.write_val(&exit_code)?;
+    }
 
     Ok(SyscallReturn::Return(pid as isize))
 }
