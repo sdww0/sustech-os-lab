02d59c3912519f767f974b4da9809214b1424cfc
Yuke Peng <pykfirst@gmail.com>
2025-09-12 14:45:11 +0800

    lab3-setup

---
+++ lab3-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt-get install -y --no-install-recommends libusb-dev libglib2.0-dev libpixman-1-dev meson ninja-build libgcrypt-dev libslirp-dev libevent-dev
+pip3 install yq tomli
+
+# Install qemu
+wget -O qemu.tar.xz https://download.qemu.org/qemu-8.2.1.tar.xz
+mkdir qemu
+tar -xf qemu.tar.xz -C qemu --strip-components=1
+cd qemu
+./configure --target-list=riscv64-softmmu,x86_64-softmmu --prefix=/usr/local/qemu --enable-slirp
+make -j$(nproc)
+sudo make install
+echo 'export PATH=$PATH:/usr/local/qemu/bin' >>$HOME/.bashrc
+
+cd ..
+rm -rf qemu qemu.tar.xz
+
+# Install OSDK
+sudo apt-get install -y --no-install-recommends mtools
+
+cargo install cargo-osdk --version 0.16.0
